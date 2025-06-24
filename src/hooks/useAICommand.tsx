cd20a983c2282d011edd1d5b435b33a3fab6856a
Jean Costa <jean.adcc@gmail.com>
2025-06-24 14:01:00 -0300

    chore: add logs

--- src/hooks/useAICommand.tsx
+++ src/hooks/useAICommand.tsx
@@ -12,6 +12,10 @@ export function useAICommand<T extends Object>(commandType: string) {
   >(null);
 
   useEffect(() => {
+    console.log("TextStreams changed: -> ", textStreams);
+  }, [textStreams]);
+
+  useEffect(() => {
     const latest = textStreams.at(-1);
 
     let parsed: (T & { _feId: string }) | null = null;
